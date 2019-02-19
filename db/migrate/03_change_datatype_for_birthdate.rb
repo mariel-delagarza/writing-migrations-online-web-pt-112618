a5c6e7faa85709d7dcce60b4168ea4f2b7fe047f
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-02-19 17:58:52 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,5 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
-  def change_column(table_name, column_name, type)
-    add_column :students, :grade, :integer 
-    add_column :students, :birthdate, :string 
+  def change
+    change_column :students, :birthdate, :datetime  
   end
 end
